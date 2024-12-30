bb7356a8e6a45bbe8256cf4a9815a17a46ec7f7f
Anton Ždanov <anton@azdanov.dev>
2024-12-30 15:06:32 +0200

    add go-chi

--- main.go
+++ main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello, World!</h1>")
-}
+var email = "contact@example.com"
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Home Page</h1>\n<p>Welcome to the home page!</p>")
+	})
+
+	r.Get("/contact", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Contact Page</h1>\n<p>To get in touch, please send an email to <a href=\"mailto:%s\">%s</a>.</p>", email, email)
+	})
 
 	fmt.Println("Server is running on port :3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
