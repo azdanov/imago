03378af118904f5f9e7f82f48749d79aae26b3b3
Anton Ždanov <anton@azdanov.dev>
2024-12-31 08:23:09 +0200

    add faq

--- main.go
+++ main.go
@@ -14,14 +14,28 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Home Page</h1>\n<p>Welcome to the home page!</p>")
-	})
-
-	r.Get("/contact", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Contact Page</h1>\n<p>To get in touch, please send an email to <a href=\"mailto:%s\">%s</a>.</p>", email, email)
-	})
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
 
 	fmt.Println("Server is running on port :3000")
 	http.ListenAndServe(":3000", r)
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Home Page</h1>\n<p>Welcome to the home page!</p>")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Contact Page</h1>\n<p>To get in touch, please send an email to <a href=\"mailto:%s\">%s</a>.</p>", email, email)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `<h1>FAQ</h1>
+	<dl>
+		<dt>What is this service?</dt>
+		<dd>This is a sample FAQ page.</dd>
+		<dt>How can I contact support?</dt>
+		<dd>You can contact support by sending an email to <a href="mailto:%s">%s</a>.</dd>
+	</dl>`, email, email)
 }
