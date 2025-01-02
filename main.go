42444f31daab0adbbb70e72c7deb91aa3d1b3359
Anton Ždanov <anton@azdanov.dev>
2025-01-02 15:09:41 +0200

    add static handler factory function

--- main.go
+++ main.go
@@ -5,42 +5,38 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/azdanov/imago/controllers"
 	"github.com/azdanov/imago/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-var (
-	email     = "contact@example.com"
-	templates map[string]views.Template
-)
+var email = "contact@example.com"
 
 func main() {
-	var err error
-	templates, err = views.LoadTemplates()
-	if err != nil {
-		log.Fatalf("load templates: %v", err)
-	}
-
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	tpl, err := views.Parse("home")
+	if err != nil {
+		log.Fatalf("load template: %v", err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("contact")
+	if err != nil {
+		log.Fatalf("load template: %v", err)
+	}
+	tpl.Data = struct{ Email string }{Email: email}
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse("faq")
+	if err != nil {
+		log.Fatalf("load template: %v", err)
+	}
+	tpl.Data = struct{ Email string }{Email: email}
+	r.Get("/faq", controllers.StaticHandler(tpl))
 
 	fmt.Println("Server is running on port :3000")
 	http.ListenAndServe(":3000", r)
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	templates["home"].Execute(w, nil)
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	templates["contact"].Execute(w, struct{ Email string }{email})
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	templates["faq"].Execute(w, struct{ Email string }{email})
 }
--- views/template.go
+++ views/template.go
@@ -10,29 +10,16 @@ import (
 
 type Template struct {
 	htmlTpl *template.Template
+	Data    any
 }
 
-func LoadTemplates() (map[string]Template, error) {
-	templateNames := []string{"home", "contact", "faq"}
-
-	templates := make(map[string]Template)
-	for _, n := range templateNames {
-		t, err := parse(n + ".tmpl.html")
-		if err != nil {
-			return nil, err
-		}
-		templates[n] = Template{htmlTpl: t}
-	}
-	return templates, nil
-}
-
-func parse(filename string) (*template.Template, error) {
-	p := filepath.Join("templates", filename)
+func Parse(filename string) (*Template, error) {
+	p := filepath.Join("templates", filename+".tmpl.html")
 	t, err := template.ParseFiles(p)
 	if err != nil {
 		return nil, fmt.Errorf("parse template %s: %w", filename, err)
 	}
-	return t, nil
+	return &Template{htmlTpl: t}, nil
 }
 
 func (t Template) Execute(w http.ResponseWriter, data any) {
