2c07c684a46d8b7bba359b4e8a601983fc22ad85
Anton Ždanov <anton@azdanov.dev>
2025-01-31 15:27:32 +0200

    add db support

--- main.go
+++ main.go
@@ -1,19 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/azdanov/imago/controllers"
 	"github.com/azdanov/imago/templates"
 	"github.com/azdanov/imago/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 var email = "contact@example.com"
 
 func main() {
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v", err)
+	}
+	defer db.Close()
+
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
